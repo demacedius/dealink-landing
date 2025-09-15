32da73a1011b2da0f0b051fd8fd3a1f59a22d51c
demacedius <demacedius@gmail.com>
2025-09-15 23:44:02 +0200

    finish

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://vagiudvofcosmjovxwuf.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhZ2l1ZHZvZmNvc21qb3Z4d3VmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEwOTQ5NDYsImV4cCI6MjA2NjY3MDk0Nn0.j8CeeatL7LpJI0Hzc5lax5Wt1o9LTfiyJwvH8GBKUbc'
+// @ts-ignore
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+// @ts-ignore
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
